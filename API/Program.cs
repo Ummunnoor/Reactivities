82738ad19fb854bd0dab8739c63a1b1b084051c8
Ummunnoor <99298543+Ummunnoor@users.noreply.github.com>
2025-06-05 14:02:49 +0100

    my react app

--- API/Program.cs
+++ API/Program.cs
@@ -6,9 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
-builder.Services.AddDbContext<AppDbContext>(options => {
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaulConnection"));
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -17,6 +19,9 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+   .WithOrigins("http://localhost:3000","https://localhost:3000"));
 
 
 app.MapControllers();
---
+++ Client/src/lib/types/index.d.ts
@@ -0,0 +1,12 @@
+type Activity = {
+    id : string
+    title : string
+    description : string
+    category : string
+    date : string
+    isCancelled : boolean
+    city : string
+    venue : string
+    longitude : number
+    latitude : number
+}
