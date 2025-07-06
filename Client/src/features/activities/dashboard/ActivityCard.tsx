77a3267046c8fa2293303e445cb251379bb4f117
Ummunnoor <99298543+Ummunnoor@users.noreply.github.com>
2025-07-07 00:40:55 +0100

    react query

--- Client/src/features/activities/dashboard/ActivityCard.tsx
+++ Client/src/features/activities/dashboard/ActivityCard.tsx
@@ -7,16 +7,13 @@ import {
   Chip,
   Typography,
 } from "@mui/material";
+import { useActivities } from "../../../lib/hooks/useActivities";
 type Props = {
   activity: Activity;
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
 };
-export default function ActivityCard({
-  activity,
-  selectActivity,
-  deleteActivity,
-}: Props) {
+export default function ActivityCard({ activity, selectActivity }: Props) {
+  const { deleteActivity } = useActivities();
   return (
     <Card sx={{ borderRadius: 3 }}>
       <CardContent>
@@ -33,7 +30,7 @@ export default function ActivityCard({
         sx={{ display: "flex", justifyContent: "space-between", pb: 2 }}
       >
         <Chip label={activity.category} variant="outlined" />
-        <Box display='flex' gap={3}>
+        <Box display="flex" gap={3}>
           <Button
             size="medium"
             variant="contained"
@@ -44,8 +41,9 @@ export default function ActivityCard({
           <Button
             size="medium"
             variant="contained"
-            color='error'
-            onClick={() => deleteActivity(activity.id)}
+            color="error"
+            onClick={() => deleteActivity.mutate(activity.id)}
+            disabled={deleteActivity.isPending}
           >
             Delete
           </Button>
--- Client/src/features/activities/dashboard/ActivityList.tsx
+++ Client/src/features/activities/dashboard/ActivityList.tsx
@@ -3,9 +3,9 @@ import ActivityCard from "./ActivityCard";
 type Props = {
   activities: Activity[];
   selectActivity: (id : string) => void;
-  deleteActivity: (id: string) => void;
+  
 };
-export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
+export default function ActivityList({ activities, selectActivity }: Props) {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
       {activities.map((activity) => (
@@ -13,7 +13,7 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
           key={activity.id}
           activity={activity}
           selectActivity={selectActivity}
-          deleteActivity={deleteActivity}
+         
         />
       ))}
     </Box>
